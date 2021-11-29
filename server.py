106da20d2ecd1c52e07596860df339565d9edc86
Paul <76972320+MrPaulon@users.noreply.github.com>
2021-11-29 15:52:35 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -11,20 +11,19 @@ class ClientThread(threading.Thread):
         self.clientsocket = clientsocket
         print("[+] Nouveau thread pour %s %s" % (self.ip, self.port, ))
 
-    def run(self): 
-   
+    def run(self):
+
         print("Connexion de %s %s" % (self.ip, self.port, ))
 
         r = self.clientsocket.recv(2048)
-        print("Ouverture du fichier: ", r, "...")
-        fp = open(r, 'rb')
-        self.clientsocket.send(fp.read())
+        r = r.decode("utf8")
+        print(r)
 
         print("Client déconnecté...")
 
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcpsock.bind(("127.0.0.1",1111))
+tcpsock.bind(("172.20.35.61",12086))
 
 while True:
     tcpsock.listen(10)
