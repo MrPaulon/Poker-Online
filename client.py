6af2c30540ec2f3ae845da272b31e44ba9b4b123
Paul <76972320+MrPaulon@users.noreply.github.com>
2021-11-29 15:25:33 +0100

    Ajout des scripts de communication entre machines

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("127.0.0.1", 1111))
+
+print("Le nom du fichier que vous voulez récupérer:")
+file_name = input(">> ")
+s.send(file_name.encode())
+file_name = 'data/%s' % (file_name,)
+r = s.recv(9999999)
+with open(file_name,'wb') as _file:
+    _file.write(r)
+print("Le fichier a été correctement copié dans : %s." % file_name)
