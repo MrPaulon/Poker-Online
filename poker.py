cc2aa70ffa91d38a620cd8a42a197e24680da892
Paul <76972320+MrPaulon@users.noreply.github.com>
2021-12-06 09:12:34 +0100

    Add files via upload

--- poker.py
+++ poker.py
@@ -2,28 +2,38 @@
 #import
 import random
 from random import randint
-#def cartes
+import pygame
+from pygame import *
+
+#fonctions
 cartes = [["1trefle", "1pic", "1coeur", "1careaux"], ["2trefle", "2pic", "2coeur", "2carreaux"], ["3trefle", "3pic", "3coeur", "3carreaux"], ["4trefle", "4pic", "4coeur", "4carreaux"], ["4trefle", "4pic", "4coeur", "4carreaux"], ["5trefle", "5pic", "5coeur", "5carreaux"], ["6trefle", "6pic", "6coeur", "6carreaux"], ["7trefle", "7pic", "7coeur", "7carreaux"], ["8trefle", "8pic", "8coeur", "8carreaux"], ["9trefle", "9pic", "9coeur", "9carreaux"], ["10trefle", "10pic", "10coeur", "10carreaux"], ["Vtrefle", "Vpic", "Vcoeur", "Vcarreaux"], ["Qtrefle", "Qpic", "Qcoeur", "Qcarreaux"], ["Rtrefle", "Rpic", "Rcoeur", "Rcarreaux"]]
 
+cartesPasDispo = []
+
 class Game():
-    def __init__(self, idGame):
-        self.idGame = idGame
-        self.mise = {"mise": 200}
+    plateau = []
+    def __init__(self):
+        self.mise = {"mise": 20}
 
 class Poker():
     """Class pour le Poker"""
-
     def __init__(self, nom, solde = 500):
         self.nom = nom
         self.solde = solde
         self.main = []
 
     def distribution(self):
-        for boucle in range(2):
-            nb = randint(1, 13)
-            sy = randint(1, 4)
-            print(cartes[nb][sy])
-            self.main.append[cartes[nb][sy]]
+        if len(self.main) == 2:
+            print(self.main)
+            return
+        nb = randint(0, 13)
+        sy = randint(0, 3)
+        if cartes[nb][sy] not in cartesPasDispo:
+            cartesPasDispo.append(cartes[nb][sy])
+            self.main.append(cartes[nb][sy])
+            self.distribution()
+        else:
+            self.distribution()
 
 
 
@@ -39,3 +49,32 @@ def jouer(joueur, game):
         montant = int(input("De combien voulez-vous relancez: "))
         joueur.solde -= montant
         print("Vous avez relancez avec", montant, "\nvotre solde est actuellement de", joueur.solde)
+
+#Pygame
+###Fennetre
+##pygame.init()
+##screen = pygame.display.set_mode((600, 480))
+##pygame.display.set_caption('Basic Pygame program')
+##
+### Fill background
+##background = pygame.Surface(screen.get_size())
+##background = background.convert()
+##background.fill((250, 250, 250))
+##
+###Variables
+##font = pygame.font.Font(None, 36)
+##text = font.render("Hello There", 1, (10, 10, 10))
+##textpos = text.get_rect()
+##textpos.centerx = background.get_rect().centerx
+##background.blit(text, textpos)
+##screen.blit(background, (0, 0))
+##pygame.display.flip()
+##
+### Events
+##continuer = 1
+##while continuer:
+##    for event in pygame.event.get():
+##        if event.type == QUIT:
+##            continuer = 0
+##    screen.blit(background, (0, 0))
+##    pygame.display.flip()
