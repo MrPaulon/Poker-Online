4bd441b6f31e70db6ad73c2bc3d6765ea82ecd15
Paul <76972320+MrPaulon@users.noreply.github.com>
2021-12-14 23:02:06 +0100

    Modifs poker.py + Logo

--- poker.py
+++ poker.py
@@ -38,9 +38,6 @@ class Poker():
         else:
             self.distribution()
 
-
-
-
 def jouer(joueur, game):
     print("La mise actuelle du plateau est de:", game.mise["derniereMise"])
     manche = int(input("Que voulez vous faire ? \n [1] Vous couchez \n [2] Suivre la mise\n [3] Relancer \n"))
@@ -58,29 +55,31 @@ def jouer(joueur, game):
 
 #Pygame
 ###Fennetre
-##pygame.init()
-##screen = pygame.display.set_mode((600, 480))
-##pygame.display.set_caption('Poker Online')
-##
-### Fill background
-##background = pygame.Surface(screen.get_size())
-##background = background.convert()
-##background.fill((250, 250, 250))
-##
-###Variables
-##font = pygame.font.Font(None, 36)
-##text = font.render("Hello There", 1, (10, 10, 10))
-##textpos = text.get_rect()
-##textpos.centerx = background.get_rect().centerx
-##background.blit(text, textpos)
-##screen.blit(background, (0, 0))
-##pygame.display.flip()
-##
-### Events
-##continuer = 1
-##while continuer:
-##    for event in pygame.event.get():
-##        if event.type == QUIT:
-##            continuer = 0
-##    screen.blit(background, (0, 0))
-##    pygame.display.flip()
+pygame.init()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+pygame.display.set_caption('Poker Online')
+
+# Background / Icon
+background_image = pygame.image.load("img/Table.png").convert()
+icon = pygame.image.load("img/Logo.png").convert()
+pygame.display.set_icon(icon)
+
+#Variables
+font = pygame.font.Font(None, 36)
+text = font.render("Hello There", 1, (10, 10, 10))
+textpos = text.get_rect()
+textpos.centerx = background_image.get_rect().centerx
+background_image.blit(text, textpos)
+screen.blit(background_image, (0, 0))
+pygame.display.flip()
+
+# Events
+continuer = 1
+while continuer:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continuer = 0
+    screen.blit(background_image, [0, 0])
+    pygame.display.flip()
+
+import pygame
